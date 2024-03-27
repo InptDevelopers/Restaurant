54362631aa071a4c0ffd9d004a8550eb4b58bbd1
‘ELMILYASS’ <ilyasselmazidi123@gmail.com>
2024-03-27 11:39:39 +0000

    adding technical services for microservices

--- backend/table-service/src/main/java/org/developpers/tableservice/TableServiceApplication.java
+++ backend/table-service/src/main/java/org/developpers/tableservice/TableServiceApplication.java
@@ -1,6 +1,5 @@
 package org.developpers.tableservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
