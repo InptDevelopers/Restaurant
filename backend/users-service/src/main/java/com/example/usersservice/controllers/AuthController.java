f98101915faa8ac75357b50632430ac174a700d1
‘ELMILYASS’ <ilyasselmazidi123@gmail.com>
2024-03-29 17:54:06 +0000

    .

--- backend/users-service/src/main/java/com/example/usersservice/controllers/AuthController.java
+++ backend/users-service/src/main/java/com/example/usersservice/controllers/AuthController.java
@@ -1,13 +1,13 @@
 package com.example.usersservice.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +15,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.Instant;
@@ -28,35 +29,46 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class AuthController {
-    private JwtEncoder jwtEncoder;
-    private JwtDecoder jwtDecoder;
-    private AuthenticationManager authenticationManager;
-    private UserDetailsService userDetailsService;
+        private JwtEncoder jwtEncoder;
+        private JwtDecoder jwtDecoder;
+        private AuthenticationManager authenticationManager;
+        private UserDetailsService userDetailsService;
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> jwtToken(
-            @RequestParam String email,
-            @RequestParam String password) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password));
-        String subject = authentication.getName();
-        String scope = authentication.getAuthorities()
-                .stream().map(aut -> aut.getAuthority())
-                        .collect(Collectors.toList()).get(0);
-        System.out.println(authentication.getAuthorities().toString());
-        System.out.println(authentication);
-        Map<String, String> idToken = new HashMap<>();
-        Instant instant = Instant.now();
-        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-                .subject(subject)
-                .issuedAt(instant)
-                .expiresAt(instant.plus(10, ChronoUnit.MINUTES))
-                .issuer("security-service")
-                .claim("scope", scope)
-                .build();
-        String jwtAccessToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
-        idToken.put("accessToken", jwtAccessToken);
+        @PostMapping("/login")
+        public ResponseEntity<Map<String, String>> jwtToken(
+                        @RequestParam String email,
+                        @RequestParam String password) {
+                Authentication authentication = authenticationManager.authenticate(
+                                new UsernamePasswordAuthenticationToken(email, password));
+                String subject = authentication.getName();
+                String scope = authentication.getAuthorities()
+                                .stream().map(aut -> aut.getAuthority())
+                                .collect(Collectors.toList()).get(0);
+                System.out.println(authentication.getAuthorities().toString());
+                System.out.println(authentication);
+                Map<String, String> idToken = new HashMap<>();
+                Instant instant = Instant.now();
+                JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                                .subject(subject)
+                                .issuedAt(instant)
+                                .expiresAt(instant.plus(10, ChronoUnit.MINUTES))
+                                .issuer("security-service")
+                                .claim("scope", scope)
+                                .build();
+                String jwtAccessToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+                idToken.put("accessToken", jwtAccessToken);
 
-        return new ResponseEntity<>(idToken, HttpStatus.OK);
-    }
+                return new ResponseEntity<>(idToken, HttpStatus.OK);
+        }
+
+        @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+        @GetMapping("/verifyAdmin")
+        public void validateAdmin() {
+        }
+
+        @PreAuthorize("hasAuthority('SCOPE_CLIENT')")
+
+        @GetMapping("/verifyClient")
+        public void validateClient() {
+        }
 }
